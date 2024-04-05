6c65b4fcaa0ae4b3f2603fe53564c3af5489af0c
lenox <97343775+Brownred@users.noreply.github.com>
2024-04-05 18:12:35 +0000

    modified:   card-component/src/card.jsx

--- card-component/src/card.jsx
+++ card-component/src/card.jsx
@@ -3,7 +3,7 @@ function Card(){
 
     return(
         <div className="card">
-            <img src="https://via.placeholder.com/200" alt="Just a normal image of me" />
+            <img src="https://via.placeholder.com/150" alt="Just a normal image of me" />
             <h2>Lenox Miheso</h2>
             <p>I make yotube videos. I also play video games.</p>
         </div>
