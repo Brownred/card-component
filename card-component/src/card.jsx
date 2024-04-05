ef4f84813a91f3065a669aab75c4eb84760548df
lenox <97343775+Brownred@users.noreply.github.com>
2024-04-05 18:12:03 +0000

    Learning React

---
+++ card-component/src/card.jsx
@@ -0,0 +1,13 @@
+
+function Card(){
+
+    return(
+        <div className="card">
+            <img src="https://via.placeholder.com/200" alt="Just a normal image of me" />
+            <h2>Lenox Miheso</h2>
+            <p>I make yotube videos. I also play video games.</p>
+        </div>
+    )
+}
+
+export default Card
